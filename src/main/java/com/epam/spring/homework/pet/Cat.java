12db8fc33988520a3f39086615f6aa270cae426f
Oleksandr Mykhailov <bearodessa@gmail.com>
2022-01-20 19:48:47 +0200

    Create application

---
+++ src/main/java/com/epam/spring/homework/pet/Cat.java
@@ -0,0 +1,7 @@
+package com.epam.spring.homework.pet;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat implements Animal{
+}
---
+++ src/main/java/com/epam/spring/homework/pet/Pet.java
@@ -0,0 +1,23 @@
+package com.epam.spring.homework.pet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Pet {
+
+    private List<Animal> animals;
+
+    @Autowired
+    public Pet(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public void printPets() {
+        for (Animal animal : animals) {
+            System.out.println(animal.getClass().getSimpleName());
+        }
+    }
+}
